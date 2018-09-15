f3cd2113474a2f492c1a42f35378a02f40ee48d3
cgunawan85 <33373856+cgunawan85@users.noreply.github.com>
2018-09-15 08:47:11 +0700

    CHANGES

--- courses/models.py
+++ courses/models.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from django.db import models
 from django.contrib.auth.models import User
 
-# Testing VCS
+# This is just a test
 
 STATUS_CHOICES = (
     ('i', 'In Progress'),
